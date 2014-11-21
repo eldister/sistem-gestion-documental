256ef33adeaccd3724f8160dc742cc0b005b3a96
javierbelalcaza@unicauca.edu.co <javierbelalcaza@unicauca.edu.co@4af5a635-0bcc-2537-f391-30f2da4d3ac8>
2014-11-21 13:58:46 +0000

    se agrego la autenticacion de usuarios, se crearon a paritir de sb-admin tres platillas para sar usadas en las interfaces de cada roll, sb-admin es la platilla para administradores, lector para lectores y editor para editores :P

--- trunk/default/app/controllers/usuariocrud_controller.php
+++ trunk/default/app/controllers/usuariocrud_controller.php
@@ -1,37 +1,47 @@
 <?php
+
 Load::models('usuario');
- class UsuariocrudController extends AppController
-    {
-     public function index($page = 1) {
-       
+
+class UsuariocrudController extends AppController {
+
+    public function index($page = 1) {
+
         $this->titulo = "Bienvenidos a Usuario"; // Pasa el titulo de la pagina
-        $usuario = new Usuario();         
-       // $this->listEmpleado = $empleado->getEmpleado($page);
-         $this->inner = $usuario->getUsuarios($page);
-        
+
+        $usuario = new Usuario();
+        // $this->listEmpleado = $empleado->getEmpleado($page);
+        $this->inner = $usuario->getUsuarios($page);
+
         //$this->create();
     }
-       public function edit($id)
-   {
-       $usuario = new Usuario();
-       
-       $this->titulo = "Editar Usuario";
-       //se verifica si se ha enviado el formulario (submit)
-       if(Input::hasPost('Usuario')){
- 
-           if(!$usuario->update(Input::post('Usuario'))){
-               Flash::error('Falló Operación');
-           } else {
-               Flash::valid('Operación exitosa');
-               //enrutando por defecto al index del controller
-               return Router::redirect();
-           }
-       } else {
-           //Aplicando la autocarga de objeto, para comenzar la edición
-           $this->Usuario = $usuario->find((int)$id);
-       }
-   }
-   
-     
-     
+
+    public function edit($id) {
+        $usuario = new Usuario();
+
+        $this->titulo = "Editar Usuario";
+        //se verifica si se ha enviado el formulario (submit)
+        if (Input::hasPost('Usuario')) {
+
+            if (!$usuario->update(Input::post('Usuario'))) {
+                Flash::error('Falló Operación');
+            } else {
+                Flash::valid('Operación exitosa');
+                //enrutando por defecto al index del controller
+                return Router::redirect();
+            }
+        } else {
+            //Aplicando la autocarga de objeto, para comenzar la edición
+            $this->Usuario = $usuario->find((int) $id);
+        }
     }
+
+    protected function before_filter() {
+        // Verificando si el rol del usuario actual tiene permisos para la acción a ejecutar
+        if (!$this->acl->is_allowed($this->userRol, $this->controller_name, $this->action_name)) {
+            Flash::error("Acceso negado");
+            return Router::redirect("administrador/inicioadmin", '3');
+            
+        }
+    }
+
+}
--- trunk/default/app/libs/app_controller.php
+++ trunk/default/app/libs/app_controller.php
@@ -16,12 +16,35 @@ require_once CORE_PATH . 'kumbia/controller.php';
  */
 class AppController extends Controller
 {
+    public $acl; //variable objeto ACL
+	public $userRol = ""; //variable con el rol del usuario autenticado en la aplicación
 
     final protected function initialize()
     {
         View::template('sbadmin'); /// para el template, todos los controladores que invoquen el appcontroller se va a invocar la plantilla
         //esto evita que en cada controlador se haga llamado de la  plantilla
         //Si en otro controlador hago llamado a otra plantilla, se sobreescribira esta linea
+        if(Auth::is_valid()) $this->userRol = Auth::get("rol");
+ 
+		$this->acl = new Acl();
+		//Se agregan los roles
+		$this->acl->add_role(new AclRole('lector')); // Visitantes
+		$this->acl->add_role(new AclRole('editor')); // Administradores
+		$this->acl->add_role(new AclRole('administrador')); // Usuarios
+                $this->acl->add_role(new AclRole(''));
+ 
+		//Se agregan los recursos osea todos los controladores con sus funciones
+                $this->acl->add_resource(new AclResource('usuario'), 'ingresar');
+		$this->acl->add_resource(new AclResource('usuariocrud'),'index','edit');
+                
+		 
+ 
+		//Se crean los permisos osea va 'tipoUsuario o rol','nombre_controlador' array('funcion1','funcion2','y las que quieran')
+		 // Inicio
+		$this->acl->allow('administrador', 'usuariocrud', array('index','edit'));
+		$this->acl->allow('editor','usuariocrud', array('edit'));
+                $this->acl->allow("lector", 'usuariocrud', array('index'));
+                $this->acl->allow('', 'usuario', array('ingresar'));
 
     }
 
