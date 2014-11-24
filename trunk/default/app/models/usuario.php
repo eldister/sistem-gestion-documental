9568c0c6096fba5f43afcae4969db962223534c1
jlr@unicauca.edu.co <jlr@unicauca.edu.co@4af5a635-0bcc-2537-f391-30f2da4d3ac8>
2014-11-24 04:50:26 +0000

    se agrego, en usuariocrud tanto en la vista como en el controlador el codigo para listar segun el estado, fata sacar el valor seleccionado en el combo de la vista

--- trunk/default/app/models/usuario.php
+++ trunk/default/app/models/usuario.php
@@ -6,8 +6,12 @@ class Usuario extends ActiveRecord
         $this->validates_numericality_of("EXTENSION");
         $this->validates_length_of("CONTRASENA", 50, 8);
     }
-     public function getUsuarios($page, $ppage = 5) {
-        return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
+     public function getUsuarios($pages, $ppage = 5) {
+       $usua = new Usuario();
+        $consulta = "'inactivo'";
+        // $usuario-> find_all_by_sql("SELECT * FROM `usuario` WHERE `estado`='inactivo'" );
+        //return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
+        return  $page = $this-> paginate_by_sql( "SELECT * FROM `usuario` WHERE `estado`=".$consulta , 'per_page: 5', "page: 1");
   
 
 }}
