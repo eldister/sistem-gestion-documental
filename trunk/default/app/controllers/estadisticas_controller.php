aa12c4372f25c657dc6dcb5b0df336953eb1c1e4
steeven@unicauca.edu.co <steeven@unicauca.edu.co@4af5a635-0bcc-2537-f391-30f2da4d3ac8>
2014-12-09 23:30:59 +0000

    vista estadisticas

--- trunk/default/app/controllers/estadisticas_controller.php
+++ trunk/default/app/controllers/estadisticas_controller.php
@@ -4,17 +4,22 @@ class EstadisticasController extends AppController {
     
     public function index(){
         
-        View::template('default');
+        //View::template('sbadmin');
         $Usuario = new Usuario();
+        $vector[][] = null;        
+        $fila = -1;
         foreach($Usuario->find_all_by_sql("select * from usuario where fechanacimiento_at between '2014-03-14' and '2014-12-19'") as $usuario){
-            print_r($usuario->nombre);
+            $col = 0;
+            $vector[$fila += 1][$col] = $usuario->nombre;
+            $vector[$fila][$col += 1] = $usuario->apellido;
         }
-        print_r($usuario->array[0][2]);
+        print_r($vector);
     }
     
-    public function ejemplo(){
+    public function docmasleidos(){
         
         View::template('sbadmin');
+        
         
     }
 
